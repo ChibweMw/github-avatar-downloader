8c30e77bf82d53873b8af353266bfcfdc6d4d383
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-05-15 00:21:10 +0000

    Added Headers to the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,17 @@
 var request = require('request');
+var secrets = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      Authorization: "9c310b03b5ce1ef57a0d354818d2181ee88d8448";
+    },
+  };
+
   request(url, function(err, res, body) {
     cb(err, body);
   });
